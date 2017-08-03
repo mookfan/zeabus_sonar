1fca3bf0982c1a86178dc7d8d47806816d5bcbb0
mookfan <mookfan@gmail.com>
2017-08-03 22:11:08 +0700

    Update sonar_corner.py

--- sonar/src/sonar_corner.py
+++ sonar/src/sonar_corner.py
@@ -21,9 +21,11 @@ index = 0
 p0 = [],
 
 Range = 20 #!!
-dis_obj = 2.8 #!!
-r_thres = 20 #!!
-bin_thres = 80 #!!
+dis_obj = 2.6	#Navigate = 1.8, 2.6
+r_thres = 17 #!!
+bin_thres = 90	#Navigate = 75
+theta_out = 0
+angle_thres = 40
 #r_out, theta_out = [], []
 
 r_met = 0
@@ -34,12 +36,12 @@ mask = None
 
 def Findobject():
 	global p0, Img_frame, Img_sh, r_out, theta_out, min_dis, max_dis
-	global Range, dis_obj, r_thres, count
+	global Range, dis_obj, r_thres,angle_thres, count
 	#r_out, theta_out = [], []
 	dis_pos = []
 	row, col = Img_frame.shape[0], Img_frame.shape[1]
 	x_ref, y_ref = col/2, row
-	min_dis, max_dis = 2/(float(Range/y_ref)), dis_obj/(float(Range/y_ref)) 
+	min_dis, max_dis = 1.1/(float(Range/y_ref)), dis_obj/(float(Range/y_ref)) 
 	chk = False
 
 	if count % 5 == 0:
@@ -48,52 +50,48 @@ def Findobject():
 
 	for i in range (0,len(p0)):
 		x, y = p0[i][0][0], p0[i][0][1]
-		theta_ = ((math.atan2((x_ref - x), (y_ref - y))) * 360) / (2 * np.pi)
-		r_met, theta_met = meters(x, y, x_ref, y_ref)
+		r_met = meters(x, y, x_ref, y_ref)
 		if r_met <= r_thres:
-			#print "Estimate # %d frame Object # %d: R = %.2f meters, Theta = %.2f" % (count, j + 1, r_met, theta_met)
 			cv2.circle(Img_frame, (x, y), 5, (102, 255, 204), -1)
 			dis_pos.append((x, y))
 	#pubIm(Img_frame)
 
 	for i in range (0,len(dis_pos)):
 		x, y = dis_pos[i][0], dis_pos[i][1]
-		theta = 0
 		cv2.circle(Img_frame, (x, y), 5, (102, 255, 204), -1)
-		for j in range (0, len(dis_pos)):
-			xi, yi = dis_pos[j][0], dis_pos[j][1]
-			distance = np.sqrt(np.power(x - xi, 2) + np.power(y - yi, 2))
-			"""print "===min max==="
-			print min_dis, max_dis
-			print "==dis=="
-			print distance
-			print "===xy==="
-			print x, y
-			print xi, yi"""
-			if min_dis <= distance <= max_dis:
-				if x < xi:
-					if y < yi: 
-						#print "1"
-						theta = 180-((math.atan2((yi - y), (xi - x))) * 360) / (2 * np.pi)
-					else: #y > yi
-						#print "2"
-						theta = ((math.atan2((y - yi), (xi - x))) * 360) / (2 * np.pi)
-				else: #x > xi
-					if y < yi:
-						#print "3"
-						theta = ((math.atan2((yi - y), (x - xi))) * 360) / (2 * np.pi)
-					else: #y > yi
-						#print "4"
-						theta = 180-((math.atan2((y - yi), (x - xi))) * 360) / (2 * np.pi)
-				if (0 <= theta <= 40 or 130 <= theta <= 180):
-				  	cv2.line(Img_frame, (x, y), (xi, yi), (255, 212, 128), 2)
-					chk = True
-					print "=======theta====="
-					print theta
-	pubIm(Img_frame)
+		##__LEFT(+)__RIGHT(-)__##
+		angle = ((math.atan2((x_ref - x), (y_ref - y))) * 360) / (2 * np.pi)
+		##__Limit__ANGLE___##
+		if -(angle_thres) <= angle <= angle_thres:		
+			print angle
+			for j in range (0, len(dis_pos)):
+				xi, yi = dis_pos[j][0], dis_pos[j][1]
+				distance = np.sqrt(np.power(x - xi, 2) + np.power(y - yi, 2))			
+				if min_dis <= distance <= max_dis:
+					if x < xi:
+						if y < yi: 
+							#print "1"
+							theta = 180-((math.atan2((yi - y), (xi - x))) * 360) / (2 * np.pi)
+						else: #y > yi
+							#print "2"
+							theta = ((math.atan2((y - yi), (xi - x))) * 360) / (2 * np.pi)
+					else: #x > xi
+						if y < yi:
+							#print "3"
+							theta = ((math.atan2((yi - y), (x - xi))) * 360) / (2 * np.pi)
+						else: #y > yi
+							#print "4"
+							theta = 180-((math.atan2((y - yi), (x - xi))) * 360) / (2 * np.pi)
+					if (0 <= theta <= 45 or 135 <= theta <= 180):
+						cv2.line(Img_frame, (x, y), (xi, yi), (255, 212, 128), 2)
+						chk = True
+						#print "=======theta====="
+						theta_out = theta
+						#print theta
 	if chk:
 		print "Can't find"
-	return theta, chk
+	pubIm(Img_frame)
+	return theta_out, chk
 
 
 def CornerDetect(Img_gray):
@@ -128,7 +126,28 @@ def meters(x,y,x_ref, y_ref):
 	r = np.sqrt(np.power(x_met - x_ref, 2) + np.power(y_met - y_ref, 2))
 	theta = ((math.atan2((x_ref - x_met), (y_ref - y_met))) * 360) / (2 * np.pi)
 	theta = theta
-	return r, theta
+	return r
+
+def Convolution(Img_conv):
+
+	kernel = np.ones((9,9))
+	#print kernel
+	for i in range (0,3):
+		for j in range (0,3):
+			kernel[i+2][j+2] = 0
+
+	# print kernel
+	"""
+	y,x = np.ogrid[-2: 2+1, -2: 2+1]
+	kernel = x**2+y**2 <= 1**2
+	kernel = kernel.astype(float)
+	kernel = np.abs(kernel - 1)
+	print kernel"""
+
+	rth, Img_conv = cv2.threshold(Img_conv, bin_thres, 255, 0)
+	Img_conv = cv2.filter2D(Img_conv,-1,kernel/49)
+	rth, Img_conv = cv2.threshold(Img_conv, 40, 255, 0)
+	return Img_conv
 
 def Process():
 	global Img_frame, p0, count
@@ -142,13 +161,19 @@ def Process():
 		continue
 
 	count += 1
+	print count
 	left = int(round((Img_frame.shape[1]/2)-(Img_frame.shape[1]/4)))
 	right = int(round((Img_frame.shape[1]/2)+(Img_frame.shape[1]/4)))
 	
 	Img_gray = cv2.cvtColor(Img_frame, cv2.COLOR_BGR2GRAY)
 
+	if count == 1:
+		print ("++")
+		cv2.imwrite("/home/johny/Desktop/im.jpg", Img_gray)
+
+	Img_conv = Convolution(Img_gray)
 	#print "Corner", count
-	p0, mask = CornerDetect(Img_gray)
+	p0, mask = CornerDetect(Img_conv)
 	#print "Find Object"
 	tt, st = Findobject()
 	tt = int(round(tt))
